69ca66705add88035625e3cc337ced965a4b032a
dtonderski <dtonderski@gmail.com>
2023-05-05 13:01:44 +0200

    Don't ignore neuralodes/data

---
+++ neuralodes/data/mnist.py
@@ -0,0 +1,28 @@
+import torchvision
+from torch.utils.data import DataLoader
+
+def get_normalization() -> torchvision.transforms.Compose:
+    return torchvision.transforms.Compose(
+        [
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.5,), (0.5,)),
+        ]
+    )
+
+def get_dataset(train: bool, root: str = "data/MNIST") -> torchvision.datasets.MNIST:
+    return torchvision.datasets.MNIST(
+        root,
+        train=train,
+        download=True,
+        transform=get_normalization(),
+    )
+
+def get_dataloader_from_dataset(dataset: torchvision.datasets.MNIST, batch_size: int = 128) -> DataLoader:
+    return DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        shuffle=True
+    )
+
+def get_dataloader_split(train: bool, batch_size: int = 128) -> DataLoader:
+    return get_dataloader_from_dataset(get_dataset(train), batch_size)
